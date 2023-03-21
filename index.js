51e6a93054ddeb73e6a0901942a0e5b3551867b2
Richard Ngo <richard1ngo@gmail.com>
2023-03-21 17:57:11 -0400

    mod routes to accept GET req

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config();
 
+const inventoryRoutes = require('./routes/inventory');
+const warehousesRoutes = require('./routes/warehouses');
 
 const PORT = process.env.PORT;
 const CLIENT_URL = process.env.CLIENT_URL;
@@ -14,8 +16,12 @@ app.use(cors(
 app.use(express.json());
 
 app.get('/', (_req, res) => {
-    res.send(console.log("🔥 GET Success!"));
+    res.status(200).send(console.log("🔥 GET Success!"));
 });
+
+app.use('/inventory', inventoryRoutes);
+app.use('/warehouses', warehousesRoutes);
+
 
 app.listen(PORT, () => {
     console.log(`🚀 Server listening on ${PORT}`);
