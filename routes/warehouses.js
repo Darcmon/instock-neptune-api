6f005b6277513a527de570daeda638aa9063e67d
Michael Deng <michaeldeng55@gmail.com>
2023-03-23 15:37:42 -0400

    successfully posted new warehouse

--- routes/warehouses.js
+++ routes/warehouses.js
@@ -10,4 +10,12 @@ router.get('/', (_req, res) => {
     res.status(200).send(console.log("🔥 GET/warehouses Success!"));
 });
 
+router.route('/add').post(warehouseController.addWarehouse)
+
+// router.post('/add', (req, res) => {
+//     console.log(req.body);
+//     const {warehouseName, warehouseAddress, warehouseCity, warehouseCountry, contactName, contactPosition, contactNumber, contactEmail} = req.body
+//     res.status(200).send('Inside the post')
+// })
+
 module.exports = router;
