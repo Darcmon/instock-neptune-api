48e40911da443e5b05be89dcd8230b51855565fe
Richard Ngo <richard1ngo@gmail.com>
2023-03-22 20:07:50 -0400

    inventory list including warehouse_name done

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -3,14 +3,16 @@ const knex = require("knex")(require("../knexfile"));
 exports.index = (_req, res) => {
   knex
     .select(
-      "id",
-      "item_name",
-      "description",
-      "category",
-      "status",
-      "quantity"
+      "inventories.id",
+      "inventories.item_name",
+      "inventories.description",
+      "inventories.category",
+      "inventories.status",
+      "inventories.quantity",
+      'warehouses.warehouse_name'
     )
     .from("inventories")
+    .join('warehouses', 'inventories.warehouse_id', '=', 'warehouses.id')
     .then((data) => {
       res.status(200).json(data);
     })
